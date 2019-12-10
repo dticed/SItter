276bc687276e20a6d0192e948fe3a9bd7c810ec4
dticed <lucaspierrealencar@gmail.com>
2019-12-10 01:34:07 -0300

    add email function

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -129,3 +129,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'sitter-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "ifcecratosi@gmail.com"
+EMAIL_HOST_PASSWORD = "projetoap1"
--- django_project/sitter/views.py
+++ django_project/sitter/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -15,25 +19,34 @@ def home(request):
 def home2(request):
     return render(request, 'sitter/home2.html')
 
+
 class PostListView(LoginRequiredMixin, ListView):
-    model = Post 
+    model = Post
     template_name = 'sitter/home.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted'] #ordena por data
+    ordering = ['-date_posted']  # ordena por data
+
 
 class PostDetailView(DetailView):
-    model = Post 
+    model = Post
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post 
+    model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
+        emails = []
+        for user in User.objects.all():
+            emails.append(user.email)
+        model = Post
         form.instance.author = self.request.user
+        send_mail(form.instance.title, "E-mail para turma de "+ str(form.instance.author) +"\n" + form.instance.content, 'ifcecratosi@gmail.com', emails)
         return super().form_valid(form)
 
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post 
+    model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
@@ -45,6 +58,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True
         return False
+
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
@@ -55,3 +69,13 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
+
+#def sendmail(request):
+#        send_mail(
+#        model.title,
+#        model.content,
+#        'ifcecratosi@gmail.com',
+#        ['lucaspierrealencar@gmail.com'],
+#    )
+#        return HttpResponse("Enviado")
+
